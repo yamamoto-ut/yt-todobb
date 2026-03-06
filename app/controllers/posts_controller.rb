960390d85154dadf80173a2748e726a9f31e62f2
yamamoto <wdjtm423@gmail.com>
2026-03-07 00:07:45 +0900

    Add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
     def index
-        
+        #postsでアクセスしたときの処理データベースから全ての投稿を取得して@postsに格納
+        @posts = Post.all
+    end
+
+    def new
+        #新しい投稿を作成するためのフォームを表示するためのアクション
+        @post = Post.new
     end
 end
